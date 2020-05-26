7a57bbe800d55d481f8b6ae1aa416e24e46e3853
mutwakilmo <mutwakilmo@gmail.com>
2020-05-26 16:13:28 +0200

    Initial commit

---
+++ app/src/main/java/com/mutwakilmo/tabexperiment/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mutwakilmo.tabexperiment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+}
